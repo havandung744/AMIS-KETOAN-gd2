c19b4bf7f613c2e97978b2a79411517e58a09845
hà văn dũng <83910596+havandung744@users.noreply.github.com>
2022-07-26 15:51:48 +0700

    update code

--- amis-ketoan/src/js/miSaResource.js
+++ amis-ketoan/src/js/miSaResource.js
@@ -10,6 +10,7 @@ export const miSaResource = {
         function: "Chức năng đang phát triên!",
         deleteMany: "Bạn có thực sự muốn xóa những nhân viên đã chọn không?",
         infoNotNull: "Thông tin không được để trống",
-        InvalidEmailFormat: "Email không đúng định dạng"
+        InvalidEmailFormat: "Email không đúng định dạng",
+        checkgit: "test github"
     }
 };
