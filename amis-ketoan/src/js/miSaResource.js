b9e458644ebc7f5513d3bf760b5c8d2a15982e5d
Hà Văn Dũng <83910596+havandung744@users.noreply.github.com>
2022-08-02 10:37:36 +0700

    Update miSaResource.js

--- amis-ketoan/src/js/miSaResource.js
+++ amis-ketoan/src/js/miSaResource.js
@@ -16,6 +16,5 @@ export const miSaResource = {
         infoNotNull: "Thông tin không được để trống",
         InvalidEmailFormat: "Email không đúng định dạng",
         InvalidIdentityFormat: "Số căn cước công dân không đúng định dạng",
-        phone: "test"
     }
 };
