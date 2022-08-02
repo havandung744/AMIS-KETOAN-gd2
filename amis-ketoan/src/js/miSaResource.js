dd02d3cd8ecda34cd73d8971d99b53455422ad68
Hà Văn Dũng <83910596+havandung744@users.noreply.github.com>
2022-08-02 10:51:23 +0700

    Update miSaResource.js

--- amis-ketoan/src/js/miSaResource.js
+++ amis-ketoan/src/js/miSaResource.js
@@ -1,5 +1,20 @@
 export const miSaResource = {
     VI: {
-       
+        EmployeeCodeNotNull: "Mã nhân viên không được phép để trống.",
+        EmployeeCodeDuplicate: "Mã nhân viên đã tồn tại trong hệ thống.",
+        EmployeeNameNotNull: "Tên nhân viên không được phép để trống.",
+        DepartmentNameNotNull: "Tên phòng ban không được phép để trống.",
+        EmployeePositionNotNull: "Chức vụ không được phép để trống.",
+        OrganizationNameNotNull: "Tên tổ chức không được phép để trống",
+        TaxCodeNotNull: "Mã số thuế không được phép để trống",
+        OrganizationAddressNotNull: "Địa chỉ tổ chức không được phép để trống",
+        CheckDateOfBirth: "Ngày sinh không được lớn hơn ngày hiện tại.",
+        CheckIdentityDate: "Ngày cấp không được lớn hơn ngày hiện tại.",
+        CheckEmail: "Email không đúng định dạng.",
+        function: "Chức năng đang phát triên!",
+        deleteMany: "Bạn có thực sự muốn xóa những nhân viên đã chọn không?",
+        infoNotNull: "Thông tin không được để trống",
+        InvalidEmailFormat: "Email không đúng định dạng",
+        InvalidIdentityFormat: "Số căn cước công dân không đúng định dạng",
     }
 };
